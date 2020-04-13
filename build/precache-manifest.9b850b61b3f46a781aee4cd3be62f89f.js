ce00d013de4b94d7faca1bfc7544490c2219b341
BraianMendes <00.braian.dev@gmail.com>
2020-04-13 02:20:32 -0300

    Production to Deploy

---
+++ build/precache-manifest.9b850b61b3f46a781aee4cd3be62f89f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73069e0381cac955346dfca9c77446ef",
+    "url": "./src/index.html"
+  },
+  {
+    "revision": "9b03797055b56f8cd6e8",
+    "url": "./src/static/css/main.502c167e.chunk.css"
+  },
+  {
+    "revision": "f964c4e3ab7a514ae181",
+    "url": "./src/static/js/2.2d239d7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./src/static/js/2.2d239d7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b03797055b56f8cd6e8",
+    "url": "./src/static/js/main.43879b29.chunk.js"
+  },
+  {
+    "revision": "c0b11b3f3a297f655a94",
+    "url": "./src/static/js/runtime-main.aff3759c.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "./src/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "./src/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
