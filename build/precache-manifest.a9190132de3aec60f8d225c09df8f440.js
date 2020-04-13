a921d50ea52f898137a57d28e300ddd77bed4000
BraianMendes <00.braian.dev@gmail.com>
2020-04-13 03:25:54 -0300

    Build fixed

---
+++ build/precache-manifest.a9190132de3aec60f8d225c09df8f440.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf4d3aa7c4d38c61b0f63737e411b512",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7085136948e6f9a46252",
+    "url": "./static/css/main.502c167e.chunk.css"
+  },
+  {
+    "revision": "f964c4e3ab7a514ae181",
+    "url": "./static/js/2.2d239d7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.2d239d7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7085136948e6f9a46252",
+    "url": "./static/js/main.7ea7522d.chunk.js"
+  },
+  {
+    "revision": "a37e59596a0d38384b4f",
+    "url": "./static/js/runtime-main.a0960911.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "./static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "./static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./src/precache-manifest.9b850b61b3f46a781aee4cd3be62f89f.js"
+  "./precache-manifest.a9190132de3aec60f8d225c09df8f440.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./src/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
